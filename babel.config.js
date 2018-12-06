330f9ed54aebc831d06427d7176bf167a730659c
alim <a1imov233@gmail.com>
2018-12-06 16:14:48 +0300

    - fix promise polyfills

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,12 @@
-module.exports = {
-  presets: [
-    '@vue/app',
-  ],
-};
+const presets = [
+    ['@vue/app', {
+        polyfills: [
+            'es6.array.iterator',
+            'es6.promise',
+            'es6.symbol',
+        ],
+    }],
+];
+const plugins = [];
+
+module.exports = { presets, plugins };
